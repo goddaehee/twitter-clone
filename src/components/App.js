a4fac1102ec7dc59160ef6b50018a04017965442
mengga <zonefors@gmail.com>
2022-05-12 00:53:53 +0900

    커밋 해보기

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { auth } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObj(user);
+        const uid = user.uid;
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "Intializing.."
+      )}
+      <footer>&copy; {new Date().getFullYear()}Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { dbservice } from "fbase";
+import {
+  collection,
+  addDoc,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(dbservice, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const nweetArr = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(nweetArr);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log(`음///???:${nweet}`);
+    await addDoc(collection(dbservice, "nweets"), {
+      text: nweet,
+      createdAt: Date.now(),
+      creatoId: userObj.uid,
+    });
+
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Home;
